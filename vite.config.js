16fec057bcdd9f4a376dfb143c7a201db0de13a5
mm <yolamiller@gmail.com>
2025-12-09 18:21:55 +0800

    Change base path in vite.config.js to root

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 👇 這一行非常重要，左右要有斜線
-  base: '/yodascience-portal/', 
+  // 👇 因為你有買網址，所以這裡必須改成「根目錄」
+  base: '/', 
 })
